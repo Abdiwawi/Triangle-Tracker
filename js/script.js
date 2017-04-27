686ebdb878b1070a958eb87afbd3446330a6c591
Abdiwawi <abdijamzilan@gmail.com>
2017-04-27 20:14:49 +0300

    add varaibles

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
   $("#forms form").submit(function(event){
-      
+      var a = parseInt($("input#Sidea").val());
+      var b = parseInt($("input#sideb").val());
+      var c = parseInt($("input#sidec").val());
+      var results = "";
   }
 }
