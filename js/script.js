6fbaa3a8983434047b7d02b5bd4fda6d4882215e
Abdiwawi <abdijamzilan@gmail.com>
2017-04-27 20:26:36 +0300

    add varaibles

--- js/script.js
+++ js/script.js
@@ -4,5 +4,18 @@ $(document).ready(function() {
       var b = parseInt($("input#sideb").val());
       var c = parseInt($("input#sidec").val());
       var results = "";
+
+
+      if(a > b + c || b > a + c || c > a + b){
+        return "Not a Triangle!";
+      }
+      else if (a === b, b === c, a === c) {
+        return "Equilateral";
+      }
+      else if (a === b,) {
+
+      }
+
+      }
   }
 }
