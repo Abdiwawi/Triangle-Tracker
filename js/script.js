50a778393f7aa1083954557b41c38667cbdb3a4d
Abdiwawi <abdijamzilan@gmail.com>
2017-04-27 20:57:38 +0300

    add add working code

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
-  $("#forms form").submit(function(event){
+  $("#forms1 form").submit(function(event){
       var a = parseInt($("input#Sidea").val());
-      var b = parseInt($("input#sideb").val());
-      var c = parseInt($("input#sidec").val());
+      var b = parseInt($("input#Sideb").val());
+      var c = parseInt($("input#Sidec").val());
       var results = "";
 
 
-      if(a > b + c || b > a + c || c > a + b){
-        return "Not a Triangle!";
+      if (a+b <= c || a+c <= b || c+b <=a) {
+        results += "Not a Triangle"
       }
-      else if (a === b, b === c, a === c) {
-        return "Equilateral";
+      else if ((a===b && b!==c && c!==a) || (a!==b && b!==c && c===a) || (a!==b && b===c && c!==a)) {
+        results += "Isoscless Triangle"
       }
-      else if (a === b,) {
-
+      else if (a===b && b===c && c===a) {
+        results += "Equilateral"
       }
-
+      else {
+        results += "Scalene Triangle"
       }
-  }
-}
+    
+      document.getElementById("output").innerHTML = results;
+      event.preventDefault();
+  });
+});
